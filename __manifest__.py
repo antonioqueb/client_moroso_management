e8730e82a9e70dfa6fe416d9acb58b879e836d1b
Antonio Queb <antonioqueb@alphaqueb.online>
2025-02-07 23:43:38 -0600

    Up

--- __manifest__.py
+++ __manifest__.py
@@ -14,12 +14,11 @@
         'contacts',
     ],
     'data': [
-    'security/client_moroso_management_security.xml',
-    'security/ir.model.access.csv',
-    'views/res_partner_view.xml',
-    'views/menu.xml',
-],
-
+        'security/client_moroso_management_security.xml',
+        'security/ir.model.access.csv',
+        'views/res_partner_view.xml',
+        'views/menu.xml',
+    ],
     'images': ['static/description/icon.png'],
     'installable': True,
     'application': True,
